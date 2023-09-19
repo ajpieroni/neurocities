d4e97e9d40fc59d6ba4b964fafeb423618b89cec
Alex <akp42@duke.edu>
2023-09-19 17:36:00 -0400

    wrote main.py for tracking_data

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import csv
 
-# Define the input and output file paths
-input_file = 'lucianq1.txt'
-output_file = 'lucianq1.csv'
+# in
+input_file_tracking_data = './Dojo/_tracking_data_trial_1.txt'
+output_file_tracking_data = 'tracking_data.csv'
+import csv
 
-# Open the input and output files
-with open(input_file, 'r') as txt_file, open(output_file, 'w', newline='') as csv_file:
-    # Create a CSV writer with colon as the delimiter
-    csv_writer = csv.writer(csv_file, delimiter=':')
+# CSV header
+header = ['seconds', 'point x', 'point y', 'point z', 'position x', 'position y', 'position z', 'flag']
 
-    # Read each line from the text file and write it to the CSV file
-    for line in txt_file:
-        # Split the line by colon (":") and write to the CSV file
-        parts = line.strip().split(':')
-        csv_writer.writerow(parts)
+# list to store the data rows
+data_rows = []
 
-print(f'Conversion from {input_file} to {output_file} is complete.')
+#input
+with open(input_file_tracking_data, 'r') as file:
+    # skip header
+    next(file)
+
+    for line in file:
+        values = line.strip().split('\t')
+        data_rows.append(values)
+
+with open(output_file_tracking_data, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    
+    # header
+    writer.writerow(header)
+    
+    # rows
+    writer.writerows(data_rows)
+
+printf("Export done.")
